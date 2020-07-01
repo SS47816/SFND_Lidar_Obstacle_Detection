6c523e79f5f704ed71c2847989bef447e34e41d3
SS47816 <ss1057532022@gmail.com>
2020-07-01 15:09:21 +0800

    feat: add RANSEC

--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -55,6 +55,25 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
     auto startTime = std::chrono::steady_clock::now();
 	pcl::PointIndices::Ptr inliers;
     // TODO:: Fill in this function to find inliers for the cloud.
+    pcl::SACSegmentation<PointT> seg;
+    pcl::PointIndices::Ptr inliers {new pcl::PointIndices};
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+
+    seg.setOptimizeCoefficients{true};
+    seg.setModelType{pcl::SACMODEL_PLANE};
+    seg.setMethodType{pcl::SAC_RANSAC};
+    seg.setMaxIterations(maxIterations);
+    seg.setDistanceThreshold(distanceThreshold);
+
+    // Segment the largest planar component from the input cloud
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+    if (inliers->indices.empty())
+    {
+        std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+    }
+
+    std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> segResult
 
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
