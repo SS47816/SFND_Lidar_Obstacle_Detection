f502e49a74a4a8079903910f04bde22a8b2fb7b3
SS47816 <ss1057532022@gmail.com>
2020-07-01 11:05:29 +0800

    feat: setup the lidar and processor

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,12 +42,17 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
+    auto lidar = new Lidar(cars, 0.0);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud(lidar->scan());
+    // renderRays(viewer, lidar->position, inputCloud);
+    renderPointCloud(viewer, inputCloud, "inputCloud");
 
     // TODO:: Create point processor
+    auto pointProcessor = new ProcessPointClouds<pcl::PointXYZ>();
   
 }
 
