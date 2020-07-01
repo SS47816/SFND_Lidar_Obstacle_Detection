b47f623bdedccfdfac9d1d0eca7e8537e9d97943
SS47816 <ss1057532022@gmail.com>
2020-07-01 21:10:49 +0800

    feat: done cloud separation

--- src/environment.cpp
+++ src/environment.cpp
@@ -53,7 +53,9 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
 
     // TODO:: Create point processor
     auto pointProcessor = new ProcessPointClouds<pcl::PointXYZ>();
-  
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor->SegmentPlane(inputCloud, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
 }
 
 
